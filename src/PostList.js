f75c7ed88aa8f7f19da6fc30719ab44035b82520
Thomas Wilburn <news@thomaswilburn.net>
2017-05-17 13:00:32 -0700

    Fix the async code bug

--- src/PostList.js
+++ src/PostList.js
@@ -16,10 +16,8 @@ class PostList extends Component {
   getTopPosts() {
     // Get an array of top story IDs
     fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
-      .then((response) => {
-        // return JSON
-        return response.json();
-      }).then((data) => {
+      .then(response => response.json()).then((data) => {
+        console.log(data);
         // loop the the ids and surround them with the API url
         this.stories = data.map(function(id) {
           return 'https://hacker-news.firebaseio.com/v0/item/' + id + '.json';
@@ -29,28 +27,22 @@ class PostList extends Component {
   }
 
   // Given an array of URLs, create an array of objects
-  getPostData(obj) {
-    var postData = [];
-    for (var i = 0; i < obj.length; i++) {
-      fetch(obj[i])
-        .then((response) => {
-          return response.json();
-        }).then((data) => {
-          postData.push(data);
-        });
-    }
-    this.setState({ posts: postData });
+  async getPostData(stories) {
+    //Because these requests are async, we have to wait until they return to call setState()
+    //So let's convert the top 10 URLs to fetch->json() promises.
+    var requests = stories.slice(0, 10).map(url => fetch(url).then(response => response.json()));
+    //Now we use Promise.all() to wait for all requests to return
+    //await is a new keyword that pauses execution in async functions to wait for a promise
+    //see also: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+    var posts = await Promise.all(requests);
+    this.setState({ posts });
   }
 
   render() {
-    console.log(this.state.posts);
     const elements = this.state.posts.map((post) => {
-      debugger;
-      console.log(this.state.posts);
-      console.log(post);
       return(
-      <div>
-        <p>{post.title}</p>
+      <div key={post.id}>
+        <h2>{post.title}</h2>
         <p>This is a test</p>
       </div>)
     });
